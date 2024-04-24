49030097f58d896ebc28f96969e335a6e0619d1d
Pasindu Upeka <upekasilva2000@gmail.com>
2024-04-24 21:36:33 +0530

    controller method completed

--- src/main/java/com/myProject/QrGenerator/model/student/controller/StudentController.java
+++ src/main/java/com/myProject/QrGenerator/model/student/controller/StudentController.java
@@ -3,29 +3,33 @@ package com.myProject.QrGenerator.model.student.controller;
 
 import com.myProject.QrGenerator.model.student.Student;
 import com.myProject.QrGenerator.model.student.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/students")
 public class StudentController {
-    private  StudentService studentService;
+    private final StudentService studentService;
 
 
-
+    @GetMapping("/all")
     public ResponseEntity<List<Student>> getAllStudent(){
 
         return ResponseEntity.ok(studentService.getAllStudent());
-
-
     }
-
+    @PostMapping("/save")
     public Student addStudent( @RequestBody Student student){
-
         return studentService.addStudent(student);
     }
 
+    @GetMapping("/{id}")
+    public Student findById(@PathVariable  long id){
+        return studentService.findById(id);
+    }
 
 
 
